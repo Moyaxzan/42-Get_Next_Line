d06e309fd7e07bb0d44c1c3c6cc6cf28d6acb255
Tao <tao.spa2003@gmail.com>
2023-05-06 10:30:13 +0200

    fix multiple line in relic

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,31 @@
 /*   By: tsaint-p <tsaint-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 17:09:44 by tsaint-p          #+#    #+#             */
-/*   Updated: 2023/05/06 07:50:32 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/05/06 10:28:32 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 //get_line
-char	*get_line(int fd, char *buffer, char *relic)
+char	*get_line(int fd, char *buffer, char *relic, int *read_byte)
 {
-	int		read_byte;
 	char	*tmp;
 
-	read_byte = 1;
-	while (read_byte > 0)
+	*read_byte = 1;
+	while (*read_byte > 0 && (!relic || !ft_strchr(relic, '\n')))
 	{
-		read_byte = read(fd, buffer, BUFFER_SIZE);
-		if (read_byte == -1)
+		*read_byte = read(fd, buffer, BUFFER_SIZE);
+		if (*read_byte == -1)
 			return (0x0);
-		buffer[read_byte] = '\0';
+		buffer[*read_byte] = '\0';
 		if (!relic)
 			relic = ft_strdup("");
 		tmp = relic;
 		relic = ft_strjoin(relic, buffer);
 		free(tmp);
 		tmp = 0x0;
-		if (ft_strchr(buffer, '\n') || !read_byte)
+		if (ft_strchr(buffer, '\n') || !*read_byte)
 			break;
 	}
 	return (relic);
@@ -60,17 +59,18 @@ char	*get_next_line(int fd)
 	static char	*relic;
 	char		*line;
 	char		*buffer;
+	int		read_byte;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0x0);
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (0x0);
-	line = get_line(fd, buffer, relic);
+	line = get_line(fd, buffer, relic, &read_byte);
 	if (!line)
 		return (0x0);
 	relic = get_relic(line);
-	if (!relic || (*relic == '\0' && *line == '\0')) //not sure it always works
+	if (!relic || (*relic == '\0' && !read_byte)) //not sure it always works
 		return (0x0);
 	return (line);
 }
